aa8970ab4a088daa83d114fe7a2ddfb46564d927
Resoki <remyaubert71@gmail.com>
2023-02-06 14:51:24 +0100

    edit

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ import express from 'express'
 import router from './src/router/index.js'
 import { init } from './src/db/index.js'
 import Config from './config.js'
-
+import cors from 'cors';
 await init()
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(router)
