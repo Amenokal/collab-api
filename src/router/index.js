849f525a7c48147ddc35084d6a17d5334ab2aa49
luciencauchy <91940429+Amenokal@users.noreply.github.com>
2023-02-06 16:47:45 +0100

    update middlewares

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import AuthController from '../controllers/AuthController.js'
 import UserController from '../controllers/UserController.js'
+import { User } from '../models/User.js'
 import { verify } from '../services/jwt.service.js'
 
 const router = express.Router()
@@ -8,11 +9,13 @@ const router = express.Router()
 router.post('/login', AuthController.login)
 
 // auth middleware
-router.use((res, req, next) => {
-  if(!req.header("Authorization"))
+router.use((req, res, next) => {
+  try {
+    verify(req.header("Authorization"))
+  }
+  catch(err) {
     return res.status(401).send({msg: 'Unauthorized'})
-
-  next()
+  }
 })
 
 router.get('/user/random', UserController.getRandom)
@@ -21,12 +24,15 @@ router.get('/user/:userId', UserController.getOne)
 router.post('/logout', AuthController.logout)
 
 // admin middleware
-router.use((res, req, next) => {
-  const token = verify(req.header("Authorization"))
-  if(!token.isAdmin)
+router.use(async (req, res, next) => {
+  try {
+    const token = verify(req.header("Authorization"))
+    const user = await User.findOne({ where: { email: token.email }})
+    return user.isAdmin
+  }
+  catch(err) {
     return res.status(401).send({msg: 'Unauthorized'})
-
-  next()
+  }
 })
 
 router.post('/user', UserController.create)
